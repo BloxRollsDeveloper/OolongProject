aec8022e87705f0499ca389b6c49ad3dadde8737
BloxRollsDeveloper <46302975+BloxRollsDeveloper@users.noreply.github.com>
2025-09-27 11:37:00 +0200

    Trying to fix jump
    Please take a look whenever you can. (PlayerController)

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,49 +1,79 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement Settings")]
     public float moveSpeed = 5f;
-    public float jumpSpeed = 7f;
-
-    public bool playerIsGrounded;
-    public Transform groundCheck;
-    public LayerMask whatIsGround;
+    public float jumpForce = 10f;
     public Vector2 groundBoxSize = new Vector2(0.8f, 0.2f);
+    public LayerMask whatIsGround;
+    public Transform groundCheck;
+    public bool playerIsGrounded;
+    
 
-    private InputManager2 _input;
     private Rigidbody2D _rigidbody2D;
+    private Animator _animator;
+    private SpriteRenderer _sprite;
+    private float horizontalInput;
 
-    private void Start()
+    void Awake()
     {
-        _input = GetComponent<InputManager2>();
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _sprite = GetComponent<SpriteRenderer>();
     }
 
-    private void Update()
+    void Update()
     {
-        // Check if touching ground
-        playerIsGrounded = Physics2D.OverlapBox(groundCheck.position, groundBoxSize, 0f, whatIsGround);
+       
+        horizontalInput = Input.GetAxisRaw("Horizontal");
 
-        // Jump
-        if (_input.Jump && playerIsGrounded)
+        
+        playerIsGrounded = Physics2D.OverlapBox(transform.position, groundBoxSize, 0f, whatIsGround);
+        if (Input.GetButtonDown("Jump") && playerIsGrounded)
         {
-            Vector2 vel = _rigidbody2D.linearVelocity;
-            vel.y = jumpSpeed;
-            _rigidbody2D.linearVelocity = vel;
+            _rigidbody2D.linearVelocity = new Vector2(_rigidbody2D.linearVelocity.x, jumpForce);
+        }
+
+        
+        if (horizontalInput != 0)
+            _sprite.flipX = horizontalInput < 0;
+
+        
+        UpdateAnimation();
+        if (Physics2D.OverlapBox(groundCheck.position, groundBoxSize, 0f, whatIsGround))
+        {
+            playerIsGrounded = true;
+        }
+        else
+        {
+            playerIsGrounded = false;
+        }
+        
+        if (Input.Jump && playerIsGrounded)
+        {
+            _rigidbody2D.linearVelocityY = jumpForce;
         }
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
         // Horizontal movement
-        Vector2 vel = _rigidbody2D.linearVelocity;
-        vel.x = _input.Horizontal * moveSpeed;
-        _rigidbody2D.linearVelocity = vel;
+        _rigidbody2D.linearVelocity = new Vector2(horizontalInput * moveSpeed, _rigidbody2D.linearVelocity.y);
     }
 
-    private void OnDrawGizmos()
+    private void UpdateAnimation()
     {
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireCube(groundCheck.position, groundBoxSize);
+        _animator.SetBool("IsGrounded", playerIsGrounded);
+        _animator.SetFloat("Speed", Mathf.Abs(horizontalInput));
+        _animator.SetFloat("VerticalVelocity", _rigidbody2D.linearVelocity.y);
+    }
+    
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, groundBoxSize);
     }
 }
