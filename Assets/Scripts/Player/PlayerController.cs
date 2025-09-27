c2b39d790f4e3bf6c4cdd29f2fa474af6df08b17
David <david125794@gmail.com>
2025-09-27 10:19:27 +0200

    Fixed Player Controller

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,16 +1,15 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float jumpSpeed = 7f;
- 
+
     public bool playerIsGrounded;
     public Transform groundCheck;
     public LayerMask whatIsGround;
-    public Vector2 groundBoxSize = new Vector2(0.8f,0.2f);
-    
+    public Vector2 groundBoxSize = new Vector2(0.8f, 0.2f);
+
     private InputManager2 _input;
     private Rigidbody2D _rigidbody2D;
 
@@ -22,22 +21,29 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        // Check if touching ground
         playerIsGrounded = Physics2D.OverlapBox(groundCheck.position, groundBoxSize, 0f, whatIsGround);
-        
+
+        // Jump
         if (_input.Jump && playerIsGrounded)
         {
-            _rigidbody2D.linearVelocityY = jumpSpeed;
+            Vector2 vel = _rigidbody2D.linearVelocity;
+            vel.y = jumpSpeed;
+            _rigidbody2D.linearVelocity = vel;
         }
+    }
+
+    private void FixedUpdate()
+    {
+        // Horizontal movement
+        Vector2 vel = _rigidbody2D.linearVelocity;
+        vel.x = _input.Horizontal * moveSpeed;
+        _rigidbody2D.linearVelocity = vel;
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireCube(groundCheck.position, groundBoxSize);
-    }
-
-    private void FixedUpdate()
-    {
-        _rigidbody2D.linearVelocityX = _input.Horizontal * moveSpeed;
     }
 }
