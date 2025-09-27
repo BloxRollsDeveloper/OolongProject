d5bcc2bd1bfe13f1364a2eadafecc1d0e3741957
BloxRollsDeveloper <46302975+BloxRollsDeveloper@users.noreply.github.com>
2025-09-27 12:41:58 +0200

    Bullet update
    - Fixed Bullet update
    - Adding Bullet Cooldown soon
    - Following the design document

--- Assets/Scripts/Player/BulletController.cs
+++ Assets/Scripts/Player/BulletController.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float speed = 15f;
+    public float bulletCooldown = 3f;
+    
     public float despawnTimer = 5f;
     public Rigidbody2D _rigidbody2D; 
     void Start()
     {
         _rigidbody2D.linearVelocity = new Vector2(0f, 1f) * speed;
         Destroy(gameObject, despawnTimer);
-        _rigidbody2D.linearVelocity = new Vector2(0f, 1f) * speed;
+        //Add BulletCooldown at Void Start (WIP)
     }
 }
